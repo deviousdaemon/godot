1482b70353a9e351591e94a029aed512562567e1
deviousdaemon <deviousdaemon@gmail.com>
2026-07-15 20:01:18 -0600

    Update version.py

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 short_name = "godot"
 name = "Godot Engine"
 major = 4
-minor = 8
-patch = 0
-status = "dev"
+minor = 7
+patch = 2
+status = "rc"
 module_config = ""
 website = "https://godotengine.org"
 docs = "latest"
